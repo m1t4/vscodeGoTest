0ddd542a524fd751e8d014c5ce1a920372cbecfb
Dmitry Kagan <dmitry.kagan@imperva.com>
2017-12-08 15:45:22 +0200

    Add http router functionality

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+// Index comment
 func main() {
-	name := "Visual Studio Code"
-	fmt.Println("Welcome to", name)
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	// })
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
+
+// Index here be comment
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
